0c63ab4975c67674a44f00aeaa78385ad0689f41
Arminmow <arminmow@gmail.com>
2022-08-09 23:02:42 +0430

    Cleaner code

--- Cell.js
+++ Cell.js
@@ -1,54 +1,59 @@
 
 class Cell {
     constructor(bomb,id,width,height,parentId) {
-        this.isBomb = bomb
-        this.cellId = id
-        this.width = width
-        this.height = height
-        this.parentId = parentId
-        this.isChecked = false
-        this.isFlagged = false
-        this.addCellToParent()
+        this.isBomb = bomb;
+        this.cellId = id;
+        this.width = width;
+        this.height = height;
+        this.parentId = parentId;
+        this.isChecked = false;
+        this.isFlagged = false;
+        this.addCellToParent();
     }
 
     addCellToParent (){
-        const cellDiv = document.createElement('div')
-        cellDiv.setAttribute('id',this.cellId)
-        cellDiv.style.width = `${this.width}px`
-        cellDiv.style.height = `${this.height}px`
-        cellDiv.classList.add('valid')
+        const cellDiv = document.createElement('div');
 
-        document.getElementById(this.parentId).appendChild(cellDiv)
+        cellDiv.setAttribute('id',this.cellId);
+        cellDiv.style.width = `${this.width}px`;
+        cellDiv.style.height = `${this.height}px`;
+        cellDiv.classList.add('valid');
+
+        document.getElementById(this.parentId).appendChild(cellDiv);
     }
 
     activeCell (bombsCount){
-        document.getElementById(this.cellId).className = 'checked'
-        document.getElementById(this.cellId).innerText = bombsCount === 0 ? '' : bombsCount
-        this.isChecked = true
+        document.getElementById(this.cellId).className = 'checked';
+        document.getElementById(this.cellId).innerText = bombsCount === 0 ? '' : bombsCount;
+        this.isChecked = true;
 
     }
 
     highlightBomb (){
         if (this.isFlagged) {
-            this.removeFlag()
+            this.removeFlag();
         }
-        const bombImg = document.createElement('img')
-        bombImg.src = 'assets/bomb-noBg.png'
-        bombImg.style.width = '100%'
-        bombImg.style.height = '100%'
-        document.getElementById(this.cellId).append(bombImg)
+        const bombImg = document.createElement('img');
+
+        bombImg.src = 'assets/bomb-noBg.png';
+        bombImg.style.width = '100%';
+        bombImg.style.height = '100%';
+
+        document.getElementById(this.cellId).append(bombImg);
     }
 
     addFlag (){
-        this.isFlagged = true
-        document.getElementById(this.cellId).style.background = 'url("assets/flag.jpg")'
-        document.getElementById(this.cellId).style.backgroundSize = '100%'
+        this.isFlagged = true;
+
+        document.getElementById(this.cellId).style.background = 'url("assets/flag.jpg")';
+        document.getElementById(this.cellId).style.backgroundSize = '100%';
     }
 
     removeFlag (){
-        this.isFlagged = false
-        document.getElementById(this.cellId).style.removeProperty('background')
+        this.isFlagged = false;
+
+        document.getElementById(this.cellId).style.removeProperty('background');
     }
 }
 
-export default Cell
+export default Cell;
--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 import Game from "./Game.js";
 
-const game = new Game()
+const game = new Game();
 
-document.getElementById('initBtn').addEventListener('click',initGame)
+document.getElementById('initBtn').addEventListener('click',initGame);
 
 function initGame (){
-    const size = document.getElementById('size').value
-    const bombAmount = document.getElementById('bombAmount').value
+    const size = document.getElementById('size').value;
+    const bombAmount = document.getElementById('bombAmount').value;
 
-    document.getElementById('gameInp').remove()
+    document.getElementById('gameInp').remove();
 
-    game.start(size,bombAmount,'grid')
+    game.start(size,bombAmount,'grid');
 }
 
